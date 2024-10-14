bf0ec9ffba65a6d8391d36af5328884834961c13
Carlos Gámez <hardpx@gmail.com>
2024-10-13 19:32:19 -0600

    remove settime use to test

--- client/src/store/thunks/fetchIndividualPost.js
+++ client/src/store/thunks/fetchIndividualPost.js
@@ -6,18 +6,8 @@ const fetchIndividualPost = createAsyncThunk(
   async (id) => {
     const response = await axios.get(`http://localhost:1969/posts/${id}`);
 
-    //   DEV ONLY
-    await pause(3000);
-
     return response.data;
   }
 );
-
-// DEV ONLY
-const pause = (duration) => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, duration);
-  });
-};
 
 export { fetchIndividualPost };
--- client/src/store/thunks/fetchPosts.js
+++ client/src/store/thunks/fetchPosts.js
@@ -4,16 +4,7 @@ import axios from 'axios';
 const fetchPosts = createAsyncThunk('posts/fetch', async () => {
   const response = await axios.get('http://localhost:1969/posts');
 
-  //   DEV ONLY
-  await pause(3000);
   return response.data;
 });
-
-// DEV ONLY
-const pause = (duration) => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, duration);
-  });
-};
 
 export { fetchPosts };
